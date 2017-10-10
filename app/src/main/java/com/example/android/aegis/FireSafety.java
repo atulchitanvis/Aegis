2fceebb8772ebdf83291c96f8e986682449bea94
atulchitanvis <atulchitanvis@gmail.com>
2017-10-10 06:13:58 +0530

    Adding Files

---
+++ app/src/main/java/com/example/android/aegis/FireSafety.java
@@ -0,0 +1,71 @@
+package com.example.android.aegis;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.CardView;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+/**
+ * Created by Aishwarya on 07-10-2017.
+ */
+
+public class FireSafety extends Fragment {
+
+    View myView;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstance){
+        myView = inflater.inflate(R.layout.firesafety,container,false);
+        return myView;
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+    }
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        CardView fireMeasures = (CardView)getView().findViewById(R.id.fireMeasures);
+        CardView panFire = (CardView)getView().findViewById(R.id.panFire);
+        CardView lpgLeakage = (CardView)getView().findViewById(R.id.lpgLeakage);
+
+        fireMeasures.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getContext(),FireMeasures.class));
+            }
+        });
+
+        panFire.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getContext(),PanFire.class));
+            }
+        });
+
+        lpgLeakage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getContext(),LpgLeakage.class));
+            }
+        });
+    }
+
+}
--- app/src/main/java/com/example/android/aegis/MainActivity.java
+++ app/src/main/java/com/example/android/aegis/MainActivity.java
@@ -1,17 +1,20 @@
 package com.example.android.aegis;
 
+import android.app.PendingIntent;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.View;
 import android.support.design.widget.NavigationView;
+import android.support.v4.app.FragmentManager;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.app.NotificationCompat;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -71,8 +74,10 @@ public class MainActivity extends AppCompatActivity
     public boolean onNavigationItemSelected(MenuItem item) {
         // Handle navigation view item clicks here.
         int id = item.getItemId();
+        FragmentManager fragmentManager=getSupportFragmentManager();
 
-        if (id == R.id.fireSafety) {
+        if (id == R.id.fire) {
+            fragmentManager.beginTransaction().replace(R.id.content_main,new FireSafety()).commit();
 
         }
         else if (id == R.id.earthquake) {
@@ -82,6 +87,7 @@ public class MainActivity extends AppCompatActivity
 
         }
         else if (id == R.id.cpr) {
+            fragmentManager.beginTransaction().replace(R.id.content_main,new Cpr()).commit();
 
         }
         else if (id == R.id.sos) {
